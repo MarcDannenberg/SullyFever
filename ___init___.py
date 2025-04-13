d94c3edfa2cd7ae67abc6c68ec7484a49251f4aa
MarcDannenberg <marc@marcjasondannenberg.com>
2025-04-13 22:25:43 +0200

    Add files via upload

---
+++ ___init___.py
@@ -0,0 +1,19 @@
+# sully_engine/kernel_modules/__init__.py
+# 🧠 Sully Core Cognitive Kernel Modules
+
+from .judgment import JudgmentProtocol
+from .dream import DreamCore
+from .math_translator import SymbolicMathTranslator
+from .fusion import SymbolFusionEngine
+from .paradox import ParadoxLibrary
+from .identity import SullyIdentity
+from .codex import SullyCodex
+
+__all__ = [
+    "JudgmentProtocol",
+    "DreamCore",
+    "SymbolicMathTranslator",
+    "SymbolFusionEngine",
+    "ParadoxLibrary",
+    "SullyIdentity",
+    "SullyCodex"
